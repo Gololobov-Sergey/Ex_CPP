18de0b897883475090924bcefb570b0417124c0b
Сергій Гололобов <gololobov.serhiy@gmail.com>
2024-11-06 11:31:45 +0200

    06.11.2024

---
+++ Ex3/Ex3.cpp
@@ -0,0 +1,46 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+    /*1.Написати програму, яка обчислює суму додатних елементів
+        масиву дійсних чисел.Замість індексів використати вказівники
+        та адресну арифметику.*/
+
+    const int size = 5;
+    float arr[size];
+
+    cout << "Enter value of array: " << endl;
+    for (float* i = arr, j = 0; i != arr + size; i++, j++)
+    {
+        cout << "arr[" << j << "] = ";
+        cin >> *(i);
+    }
+    cout << endl;
+
+    cout << "Result array :" << endl;
+    for (float* i = arr; i != arr + size; i++)
+    {
+        cout << *i << " ";
+    }
+    cout << endl << endl;
+
+    float dobutok = 1;
+
+    for (float* i = arr; i != arr + size; i++)
+    {
+        if (*i > 0)
+        {
+            dobutok *= *i;
+        }
+    }
+    cout << "Dobutok = " << dobutok << endl << endl;
+
+    system("pause");
+
+}
+
